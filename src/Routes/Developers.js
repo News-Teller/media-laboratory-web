e5814007f690ee87102c5250a850703557e78a7d
Marco Romanelli <41620979+mromanelli9@users.noreply.github.com>
2021-08-16 09:23:32 +0200

    update depoyment instructions

--- src/Routes/Developers.js
+++ src/Routes/Developers.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Typography, Container, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { CodeBlock, dracula } from "react-code-blocks";
+import { CopyBlock, dracula } from "react-code-blocks";
 import { HeroContainer } from '../Components'
 import sharedStyles from '../SharedStyles';
 
-const code_1 = `git clone https://github.com/News-Teller/media-laboratory.git
-cd media-laboratory
-cp -r notebook YOUR_JUPYTER_FOLDER`;
+const code1 = `git clone https://github.com/News-Teller/media-laboratory.git`;
 
-const code_2 = `cd server
-pip install -r requirements.txt
-docker-compose up -d mongodb
-python index.py`;
+const code2 =`docker build -t medialab-notebook notebook
+docker-compose build`;
+
+const code3 = `docker-compose up -d`;
 
 const useStyles = makeStyles((theme) => ({
   ...sharedStyles(theme),
@@ -21,11 +19,14 @@ const useStyles = makeStyles((theme) => ({
 export default function Developers() {
   const classes = useStyles();
 
+  const leftWidth = 4;
+  const rightWidth = 8;
+
   return (
       <Container>
         <HeroContainer
           title="Developers"
-          subtitle="This section will help you deploying the Media Laboratory environnement in the cloud"
+          subtitle="This section will help you deploying the Media Laboratory environnement"
           backgroundColor="#67e300"
           backgroundElementsFill="%2300a372"
         />
@@ -33,41 +34,70 @@ export default function Developers() {
         <Container className={classes.contentBox} maxWidth="md">
           <Grid container spacing={3} justifyContent="center">
             <Grid item xs={12}>
-              <Typography variant="h3">Installation</Typography>
-              <Typography>This section will help you deploying the Media Laboratory environnement in the cloud.</Typography>
+              <Typography variant="h3">Deployment</Typography>
+              <Typography>
+              Follow this steps to deploy the Media Laboratory environnement locally.
+              For more information, visit the {' '}
+              <a href="https://github.com/News-Teller/media-laboratory" rel="noreferrer">GitHub repository</a>.
+              <br />
+              These instructions require{' '}
+              <a href="https://www.docker.com/" rel="noreferrer">Docker</a> {' '}
+              and <a href="https://docs.docker.com/compose/install/" rel="noreferrer">Docker Compose</a> {' '}
+              to be installed.
+              </Typography>
             </Grid>
 
-            <Grid item xs={6}>
-              <Typography variant="h4" className={classes.number}>1</Typography>
-              <Typography>Clone this repo and copy notebook folder inside your Jupyter working directory.</Typography>
+            <Grid item xs={leftWidth}>
+              <Typography variant="h4" gutterBottom className={classes.number}>1</Typography>
+              <Typography>Clone the GitHub repository</Typography>
             </Grid>
-            <Grid item xs={6}>
-              <CodeBlock
-               text={code_1}
+            <Grid item xs={rightWidth}>
+              <CopyBlock
+               text={code1}
                language={"bash"}
-               showLineNumbers={true}
+               showLineNumbers={false}
                theme={dracula}
+               codeBlock
               />
             </Grid>
 
-            <Grid item xs={6}>
+            <Grid item xs={leftWidth}>
               <Typography variant="h4" gutterBottom className={classes.number}>2</Typography>
-              <Typography>Run the server (development mode)</Typography>
+              <Typography>Build the custom docker images</Typography>
             </Grid>
-            <Grid item xs={6}>
-              <CodeBlock
-               text={code_2}
+            <Grid item xs={rightWidth}>
+              <CopyBlock
+               text={code2}
                language={"bash"}
-               showLineNumbers={true}
+               showLineNumbers={false}
                theme={dracula}
+               codeBlock
               />
             </Grid>
 
-            <Grid item xs={6}>
+            <Grid item xs={leftWidth}>
               <Typography variant="h4" gutterBottom className={classes.number}>3</Typography>
-              <Typography>Run the example notebook example_notebook.ipynb</Typography>
+              <Typography>Run the servicess</Typography>
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={rightWidth}>
+              <CopyBlock
+               text={code3}
+               language={"bash"}
+               showLineNumbers={false}
+               theme={dracula}
+               codeBlock
+              />
+            </Grid>
+
+            <Grid item xs={leftWidth+2}>
+              <Typography variant="h4" gutterBottom className={classes.number}>4</Typography>
+              <Typography>
+                Access JupyterHub at <i>http://localhost:8000</i>{' '}
+                and your visualizations at <i>http://localhost:8080/&lt;uid&gt;</i>
+
+              </Typography>
+            </Grid>
+            <Grid item xs={rightWidth-2}>
               <img src={"https://jupyter.org/assets/labpreview.png"} alt="notebook environment preview" width="75%"/>
             </Grid>
           </Grid>
