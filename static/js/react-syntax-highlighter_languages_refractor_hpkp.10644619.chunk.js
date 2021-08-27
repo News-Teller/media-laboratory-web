390a84124ca3a9f79c3e61ae5fac394fd7d736e6
Marco Romanelli <41620979+mromanelli9@users.noreply.github.com>
2021-08-27 14:25:45 +0200

    Updates

--- static/js/react-syntax-highlighter_languages_refractor_hpkp.10644619.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmedialab-web"]=this["webpackJsonpmedialab-web"]||[]).push([[54],{251:function(a,e,s){"use strict";function t(a){a.languages.hpkp={directive:{pattern:/\b(?:(?:includeSubDomains|preload|strict)(?: |;)|pin-sha256="[a-zA-Z\d+=/]+"|(?:max-age|report-uri)=|report-to )/,alias:"keyword"},safe:{pattern:/\d{7,}/,alias:"selector"},unsafe:{pattern:/\d{1,6}/,alias:"function"}}}a.exports=t,t.displayName="hpkp",t.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hpkp.10644619.chunk.js.map
